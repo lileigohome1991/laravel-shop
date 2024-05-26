86cad0c96be0a73f2cfd93f644f69fb13da97d70
limuyi666 <1846352100@qq.com>
2024-05-26 01:31:55 +0000

    初始化秒杀模型

--- app/Models/Product.php
+++ app/Models/Product.php
@@ -13,9 +13,11 @@ class Product extends Model
 
     const TYPE_NORMAL = 'normal';
     const TYPE_CROWDFUNDING = 'crowdfunding';
+    const TYPE_SECKILL = 'seckill';
     public static $typeMap = [
         self::TYPE_NORMAL  => '普通商品',
         self::TYPE_CROWDFUNDING => '众筹商品',
+        self::TYPE_SECKILL => '秒杀商品',
     ];
 
     protected $fillable = [
@@ -29,6 +31,10 @@ class Product extends Model
     public function skus()
     {
         return $this->hasMany(ProductSku::class);
+    }
+    public function seckill()
+    {
+        return $this->hasOne(SeckillProduct::class);
     }
 
     public function getImageUrlAttribute()
